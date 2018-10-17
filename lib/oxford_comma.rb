9c7fd56281bf05492c6f68a11214b76ff300ea12
beaulieudev <beaulieudev@gmail.com>
2018-10-17 00:33:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.length == 1
-    answer = array[0]
+  return  answer = array[0]
 
   elsif array == 2
-    answer = array.join(" and ")
+    return answer = array.join(" and ")
   else
-    answer =  array.length.join(", ") + "and" + array.last
+    return answer =  array.length.join(", ") + "and" + array.last
   end
 end
